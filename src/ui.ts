3e8631542946d0187cdaf07aee51b65c8083cdef
Austin Jackson <themacphage@gmail.com>
2020-03-15 03:57:45 -0500

    blessed --> cli-progress

--- src/ui.ts
+++ src/ui.ts
@@ -1,99 +1,44 @@
 
-import chalk from 'chalk'
+import fs from 'fs';
+import path from 'path';
+import * as cliProgress from 'cli-progress';
 
-import blessed from 'blessed';
-import contrib from 'blessed-contrib'
-
-import { CSVReader } from './reader'
-import { API } from './api'
-import { LineDataProvider, UploaderProvider } from './providers';
+import { UploaderProvider } from './providers';
 
 const packagejson = require('../package.json')
 
 export class UI {
     csvFiles: string[];
-    lineProvider: LineDataProvider;
     uploaderProvider: UploaderProvider;
-    
-    screen: blessed.Widgets.Screen;
-    line: any;
-    log: any;
+    multibar: cliProgress.MultiBar;
+    bars: Map<string, cliProgress.SingleBar>;
 
     constructor(csvFiles: string[]) {
         // Initialize data streams
         this.csvFiles = csvFiles;
-        this.lineProvider = new LineDataProvider(this.onRealTimeUpdate.bind(this), 5000);
         this.uploaderProvider = new UploaderProvider(csvFiles);
-
-        // Setup UI
-        this.screen = blessed.screen({
-            smartCSR: true
-        });
-        this.screen.title = packagejson.name;
-        this.screen.key(['escape', 'q', 'C-c'], function(ch: any, key: any) {
-            return process.exit(0);
-        });
-    }
-
-    onRealTimeUpdate(meta:any, data: any): void {
-        this.log.log(chalk.yellow(`upload_queue_meta: ${meta}`))
-        //let latestEntry = data.x[data.x.length-1];
-        //this.line.setData(data);
-        //this.log.log(latestEntry);
-    }
-
-    draw(): void {
-        this.line = contrib.line({
-            top: 0,
-            left: 'center',
-            width: '100%',
-            height: '50%',
-            style: {
-                line: 'yellow',
-                text: 'green',
-                baseline: 'black'
-            },
-            border: {
-                type: 'line'
-            },
-            wholeNumbersOnly: true,
-            label: 'Pending Imports'
-        })
-        this.screen.append(this.line)
-
-        this.log = contrib.log({
-            bottom: 0,
-            left: 'center',
-            width: '100%',
-            height: '80%',
-            fg: 'green',
-            selectedFg: 'green',
-            label: 'Import Log',
-            border: {
-                type: 'line'
-            },
-        });
-
-        this.uploaderProvider.each((res) => {
-            
-            let i = this.uploaderProvider.uploadedRecords;
-            let n = this.uploaderProvider.totalRecords;
-            let message = `${i/n * 100}% --- ${res.data} (${i} of ${n})`
-
-            if(res.success) {
-                this.log.log(chalk.cyan(message))
-            }
-            else {
-                this.log.log(chalk.redBright(message))
-            }
-        })
         
-        // Append our box to the screen.
-        this.screen.append(this.log);
+        // Setup UI
+        this.multibar = new cliProgress.MultiBar({
+            clearOnComplete: false,
+            hideCursor: false
+        }, cliProgress.Presets.shades_classic)
+        this.bars = new Map<string, cliProgress.SingleBar>();
+
+        // create the progress bars
+        for(let file of csvFiles) {
+            let reader = this.uploaderProvider.readers.filter(e => path.basename(e.filePath) === path.basename(file))[0]
+            this.bars.set(path.basename(file), this.multibar.create(reader.numberOfLines - 1, 0, undefined));
+        }
     }
-    
+
     render(): void {
-        this.draw()
-        this.screen.render()
+        this.uploaderProvider.each((res, filePath) => {
+            this.bars.get(path.basename(filePath))?.increment(1, { filename: path.basename(filePath) });
+
+            // let i = this.uploaderProvider.uploadedRecords;
+            // let n = this.uploaderProvider.totalRecords;
+            // let message = `${i/n * 100}% --- ${res.data} (${i} of ${n})`
+        })
     }
 }
