bc90d4335e72aee4d2f22ebb35c3f5ddfa1c017a
Austin Jackson <themacphage@gmail.com>
2020-03-20 07:15:23 -0500

    added more cli arguments

--- src/api.ts
+++ src/api.ts
@@ -10,6 +10,16 @@ export class API {
                         : 'https://us-central1-cougargrades-aefb6.cloudfunctions.net/api';
         this.accessToken = accessToken;
     }
+
+    async self(): Promise<any> {
+        let res = await fetch(`${this.baseUrl}/private/tokens/self`, {
+            method: 'get',
+            headers: {
+                'X-Access-Token': this.accessToken
+            }
+        })
+        return (res.status === 200) ? await res.json() : null;
+    }
     
     async get(endpoint: string = '/', querystring: object = {}): Promise<any> {
         let res = await fetch(`${this.baseUrl}${endpoint}?${qs.stringify(querystring)}`, {
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import program from 'commander'
 import figlet from 'figlet'
 
 import { UI } from './ui'
+import { API } from './api'
 
 const packagejson = require('../package.json')
 
@@ -14,11 +15,27 @@ program
     .description(packagejson.description)
     .arguments('<csv...>')
     .option('--host <url>','Use this host instead of the default.', '<none>')
-    .action(csv => {
+    .option('--verify','When enabled, only validate the access token and exit.', false)
+    .option('--token <access_token>','Specify your access token for upload permissions. Must have permissions to use: `PUT /api/private/CSV`. This can also be specified with the ACCESS_TOKEN environment variable.','<none>')
+    .action(async csv => {
+        if(program.verify) {
+            let api = new API(program.token, true);
+            let serverToken = await api.self();
+            if(serverToken === null) {
+                throw "This access token is invalid, or did not have permissions to access `GET /private/tokens/self`."
+            }
+            console.log(serverToken)
+            process.exit(0);
+        }
+
         const ui = new UI(csv);
         if(program.host !== '<none>') {
             ui.uploaderProvider.api.baseUrl = program.host;
         }
+        if(program.token !== '<none>') {
+            ui.uploaderProvider.api.accessToken = program.token;
+        }
+
         ui.render();
     })
     .parse(process.argv);
