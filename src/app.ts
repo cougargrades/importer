4339df74c70898929360a79d9eb4b4e63858eaee
Austin Jackson <themacphage@gmail.com>
2020-08-27 16:02:02 -0500

    removed unused references

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import chalk from 'chalk';
 import { Server } from 'http';
 const info = chalk.grey
 const success = chalk.greenBright.bold;
-const snooze = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+//const snooze = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export class App {
     csvFiles: string[];
@@ -40,7 +40,7 @@ export class App {
         this.server = app.listen(port)
         
         console.log(info(`Listening on port: http://127.0.0.1:${port}`))
-        let cp = await open(`http://127.0.0.1:${port}`)
+        await open(`http://127.0.0.1:${port}`)
         console.log(info('Launched bull-board in browser'))
 
         let i = 1
@@ -60,13 +60,10 @@ export class App {
             i++
         })
 
-        //await snooze(5000);
-
         for(;;) {
             let remaining = await this.uploaderProvider.queue.count()
             pbar_upload.update(n - remaining, { filename: 'Bull/Redis queue progress' })
             if(remaining === 0 && i >= n) {
-                //await snooze(1000);
                 mbars.stop()
                 const delta = new Date().valueOf() - start_time.valueOf()
                 const rate = n / (delta / 1000)
